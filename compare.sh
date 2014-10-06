9d95eaf85a593cdc39b2e81e37b54f493400a959
Jan Sarenik <jasan@apiary.io>
2014-10-06 18:02:50 +0200

    Add options to compare.sh

--- compare.sh
+++ compare.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
+domain=${1:-'apiary.io'}
+compare_domain=${2:-"spf-orig.$domain"}
+
 a="/$0"; a=${a%/*}; a=${a#/}; a=${a:-.}; BINDIR=`cd $a; pwd`
 PATH=$BINDIR:$PATH
 
 temp=`mktemp`
 
-despf.sh apiary.io > ${temp}-1 2>/dev/null
-despf.sh > ${temp}-2 2>/dev/null
+despf.sh $domain > ${temp}-1 2>/dev/null
+despf.sh $compare_domain > ${temp}-2 2>/dev/null
 
 trap "rm ${temp}-*" EXIT
 
-if
-  cmp ${temp}-*
-then
-  echo "Everything OK"
-else
+cmp ${temp}-* && echo "Everything OK" || {
   echo "Please update TXT records!" 1>&2
   despf.sh | mkblocks.sh
-fi
+}
