d0eef4572cf38b2e595fe150eab84534b80dc58b
Jan Sarenik <jasan@apiary.io>
2014-10-09 15:56:23 +0200

    Fix mktemp parameter to make it Mac-ompatible

--- compare.sh
+++ compare.sh
@@ -8,7 +8,7 @@ compare_domain=${2:-"spf-orig.$domain"}
 a="/$0"; a=${a%/*}; a=${a#/}; a=${a:-.}; BINDIR=`cd $a; pwd`
 PATH=$BINDIR:$PATH
 
-temp=`mktemp`
+temp=`mktemp /tmp/$$.XXXXXXXX`
 
 despf.sh $domain > ${temp}-1 2>/dev/null
 despf.sh $compare_domain > ${temp}-2 2>/dev/null
