e8379408e6d7d46b20ee9374dded998d257a42d7
Jan Sarenik <jasan@apiary.io>
2014-11-08 12:23:28 -0800

    Do not alternate the prefix with genspfzone.sh

--- genspfzone.sh
+++ genspfzone.sh
@@ -5,4 +5,4 @@ DOMAIN=${1:-'spf-orig.apiary.io'}
 a="/$0"; a=${a%/*}; a=${a#/}; a=${a:-.}; BINDIR=`cd $a; pwd`
 PATH=$BINDIR:$PATH
 
-despf.sh | simplify.sh | mkblocks.sh | mkzoneent.sh
+despf.sh | simplify.sh | mkblocks.sh apiary.io spf | mkzoneent.sh
