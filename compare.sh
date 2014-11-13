127d780d8ba3144b31427d6a440e50109c96506b
Jan Sarenik <jasan@apiary.io>
2014-11-12 17:24:18 -0800

    Suppress cmp output

--- compare.sh
+++ compare.sh
@@ -15,7 +15,7 @@ despf.sh $compare_domain | simplify.sh > ${temp}-2 2>/dev/null
 
 trap "rm ${temp}-*" EXIT
 
-cmp ${temp}-* 2>/dev/null && echo "Everything OK" || {
+cmp ${temp}-* 2>/dev/null 1>&2 && echo "Everything OK" || {
   echo "Please update SPF TXT records of $domain!" 1>&2
   exit 1
 }
