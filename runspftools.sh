3da42deb6f50f817057f6cb2fb9a45e730eefce7
Jan Sarenik <jasan@apiary.io>
2015-04-28 15:13:44 +0200

    Add implicit logging with overwrite

--- runspftools.sh
+++ runspftools.sh
@@ -5,6 +5,8 @@ a="/$0"; a=${a%/*}; a=${a#/}; a=${a:-.}; BINDIR=`cd $a; pwd`
 bash -se <<EOF
 PATH=$BINDIR:$PATH
 cd $BINDIR
+exec > $HOME/runspftools.log 2>&1
+date
 git pull
 compare.sh || despf.sh \
   | simplify.sh | mkblocks.sh \
