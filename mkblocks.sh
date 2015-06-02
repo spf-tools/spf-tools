518f67f4b4e29f5ae84f8c570232eb78f7aaf62a
Jan Sarenik <jasan@apiary.io>
2015-06-02 13:02:18 +0200

    Change in-shell string replace for sed
    Also little tweaks in logic. The tests are passing.

--- mkblocks.sh
+++ mkblocks.sh
@@ -12,8 +12,11 @@ header="v=spf1"
 policy="~all"
 delim="^"
 packet=257
-
 domain=${1:-'spf-tools.ml'}
+
+# Automated way to find which prefix to use
+# It alternates between spf and _spf, using
+# the one which is not currently being in use
 test -n "$2" || {
   nameserver=`dig +short -t NS $domain | sed 1q`
   alternate=`dig +short -t TXT $domain @$nameserver | \
@@ -22,30 +25,41 @@ test -n "$2" || {
   alternate="${alternate}spf"
 }
 prefix=${2:-"$alternate"}
-incldomain="${prefix}X.$domain"
-footer="include:$incldomain $policy"
-counter=$((1))
 
-myout() {
-  local mycounter=$2
-  echo "${1/X/$((mycounter-1))}${delim}\"$3\""
+# One-char placeholder substituted for number later
+MYX=#
+
+incldomain="${prefix}${MYX}.$domain"
+footer="include:$incldomain $policy"
+counter=$((2))
+
+mysed() {
+  sed "s/$MYX/$1/"
 }
 
-myout $domain $counter "$header ${footer/X/1}"
-counter=$((counter+1))
+myout() {
+  mycounter=${3:-'1'}
+  mystart=`echo $1 | mysed $((mycounter-1))`
+  myrest=`echo $2 | mysed $((mycounter))`
+  echo ${mystart}${delim}\"$header $myrest\"
+}
+
+# Corner case for first entry containing merely include
+myout $domain "$footer"
 
 while
   read block
 do
   blocksprev=$blocks
   test -n "$blocks" && blocks="${blocks} ${block}" || blocks=$block
-  compare="$header $blocks ${footer/X/$counter}"
+  compare="$header $blocks $footer"
   test `echo $compare | wc -c` -ge $packet && {
-    myout $incldomain $counter "$header ${blocksprev} ${footer/X/$counter}"
+    myout $incldomain "$blocksprev $footer" $counter
     blocks=$block
     counter=$((counter+1))
     test $counter -gt 10 && { echo "Too many DNS lookups!"; exit 1; }
   }
 done
 
-test -n "$blocks" && myout $incldomain $counter "$header $blocks $policy"
+# Corner case for last entry not containing any include
+test -n "$blocks" && myout $incldomain "$blocks $policy" $counter
