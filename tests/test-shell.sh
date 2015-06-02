d9c44b2560c97b2d5ec18eb2b6599f1ee254b364
Jan Sarenik <jasan@apiary.io>
2015-06-02 13:48:12 +0200

    Add tests for mkblocks and simplify
    Example test run:
    
    ```
    $ tests/test-shell.sh
    Using /bin/sh
    BASH_VERSION='3.2.57(1)-release'
    .. Testing ./mkblocks
    .. OK
    .. Testing ./simplify
    .. OK
    Using /usr/local/bin/pdksh
    KSH_VERSION='@(#)PD KSH v5.2.14 99/07/13.2'
    .. Testing ./mkblocks
    .. OK
    .. Testing ./simplify
    .. OK
    Using /bin/bash
    BASH_VERSION='3.2.57(1)-release'
    .. Testing ./mkblocks
    .. OK
    .. Testing ./simplify
    .. OK
    Using /usr/local/bin/dash
    .. Testing ./mkblocks
    .. OK
    .. Testing ./simplify
    .. OK
    Using /usr/local/bin/mksh
    KSH_VERSION='@(#)MIRBSD KSH R50 2015/04/19'
    .. Testing ./mkblocks
    .. OK
    .. Testing ./simplify
    .. OK
    ```

---
+++ tests/test-shell.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+a="/$0"; a=${a%/*}; a=${a#/}; a=${a:-.}; BINDIR=`cd $a; pwd`
+PATH=$BINDIR/..:$PATH
+cd $BINDIR
+
+for MYSH in sh pdksh bash dash ash mksh
+do
+  MYSH=`which $MYSH` || continue
+
+  echo Using $MYSH
+  $MYSH -se < test-real.sh
+done
