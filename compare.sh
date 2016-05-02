ac4c47f6c724b4d75dd8b7ad7b66c4fb421e6fc9
Jan Sarenik <jan.sarenik@ysoft.com>
2016-05-02 16:46:38 +0200

    compare.sh: Canonize usage

--- compare.sh
+++ compare.sh
@@ -17,18 +17,18 @@
 #
 ##############################################################################
 #
-# Usage: ./compare.sh [domain [compare_domain]]
+# Usage: ./compare.sh DOMAIN1 DOMAIN2
 
-domain=${1:-'spf-tools.ml'}
-compare_domain=${2:-'orig.spf-tools.ml'}
+domain1=${1:-'spf-tools.ml'}
+domain2=${2:-'orig.spf-tools.ml'}
 
 a="/$0"; a=${a%/*}; a=${a#/}; a=${a:-.}; BINDIR=$(cd $a; pwd)
 PATH=$BINDIR:$PATH
 
 temp=$(mktemp /tmp/$$.XXXXXXXX)
 
-despf.sh $domain | normalize.sh | simplify.sh > ${temp}-1 2>/dev/null
-despf.sh $compare_domain | normalize.sh | simplify.sh > ${temp}-2 2>/dev/null
+despf.sh $domain1 | normalize.sh | simplify.sh > ${temp}-1 2>/dev/null
+despf.sh $domain2 | normalize.sh | simplify.sh > ${temp}-2 2>/dev/null
 
 trap "rm ${temp}-*" EXIT
 diff -u ${temp}-1 ${temp}-2
