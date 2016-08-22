902c5eca5970d5b268586ed148bd5ba50d7dfe1c
Jan Sarenik <jajomojo@gmail.com>
2016-08-22 12:25:23 +0200

    ci-runtest.sh: Add check for required tools

--- misc/ci-runtest.sh
+++ misc/ci-runtest.sh
@@ -4,4 +4,11 @@ ODIR=$PWD/mybin
 export PATH=$ODIR:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/local/musl/bin:$HOME/bin
 
 pwd
+
+# Check for required tools
+for cmd in host awk grep sed cut
+do
+  type $cmd >/dev/null
+done
+
 tests/test-shell.sh || DEBUG=1 tests/test-shell.sh
