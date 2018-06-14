b995a0481dd55180120be1f8c85a1c9564f3488d
Jan Sarenik <jasan@jasan.tk>
2018-06-14 16:21:11 +0200

    cloudflare.sh: Read SPFTRC before checking unset variables
    Fixes #153

--- cloudflare.sh
+++ cloudflare.sh
@@ -34,6 +34,9 @@ done
 a="/$0"; a=${a%/*}; a=${a:-.}; a=${a#/}/; BINDIR=$(cd $a; pwd)
 . $BINDIR/include/global.inc.sh
 
+# Read TOKEN and EMAIL
+test -r $SPFTRC && . $SPFTRC
+
 test -n "$TOKEN" || { echo "TOKEN not set! Exiting." >&2; exit 1; }
 test -n "$EMAIL" || { echo "EMAIL not set! Exiting.">&2; exit 1; }
 
@@ -44,9 +47,6 @@ idsfile=$(mktemp /tmp/cloudflare-ids-XXXXXX)
 zonefile=$(mktemp /tmp/cloudflare-zone-XXXX)
 cat > $zonefile
 trap "rm $idsfile $zonefile" EXIT
-
-# Read TOKEN and EMAIL
-test -r $SPFTRC && . $SPFTRC
 
 apicmd() {
   CMD=${1:-'stats'}
